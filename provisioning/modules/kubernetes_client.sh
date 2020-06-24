53a33bd47400e947d5b298908e3217d88e252c7e
Daniel Kovacs <dkovacs@deasys.net>
2020-06-24 18:22:35 +1000

    added: both helm2 and helm3 are now installed. Helm3 is the default, it will be called when using just `helm`.

--- provisioning/modules/kubernetes_client.sh
+++ provisioning/modules/kubernetes_client.sh
@@ -16,8 +16,11 @@ function _psh_kubernetes_client_configure() {
     SYSTEM_PACKAGES+=(
         kubectl
     )
-    KUBERNETES_CLIENT_HELM_DOWNLOAD_URL="https://get.helm.sh/helm-v3.0.0-linux-amd64.tar.gz"
-    KUBERNETES_CLIENT_BIN_DIR=${SYSTEM_BIN_DIR}
+    KUBERNETES_CLIENT_BIN_DIR=${KUBERNETES_CLIENT_BIN_DIR:-${SYSTEM_BIN_DIR}}
+    HELM2_VERSION=${HELM2_VERSION:-2.16.9}
+    HELM3_VERSION=${HELM3_VERSION:-3.2.4}
+    KUBERNETES_CLIENT_HELM2_DOWNLOAD_URL="https://get.helm.sh/helm-v${HELM2_VERSION}-linux-amd64.tar.gz"
+    KUBERNETES_CLIENT_HELM3_DOWNLOAD_URL="https://get.helm.sh/helm-v${HELM3_VERSION}-linux-amd64.tar.gz"
 }
 
 
@@ -74,14 +77,23 @@ function _psh_kubernetes_client_setup() {
     # helm
     # -----------------------------------------------------------
 
-    if ! type helm >/dev/null 2>&1; then
-        _psh_log_info "installing helm from: ${KUBERNETES_CLIENT_HELM_DOWNLOAD_URL}"
-        # curl -sS -o /tmp/helm.tar.gz ${KUBERNETES_CLIENT_HELM_DOWNLOAD_URL}
+    if ! type helm3 >/dev/null 2>&1; then
+        _psh_log_info "installing helm3 from: ${KUBERNETES_CLIENT_HELM3_DOWNLOAD_URL}"
         mkdir -p /tmp/helm
-        curl -sS --location ${KUBERNETES_CLIENT_HELM_DOWNLOAD_URL} | tar xz -C /tmp/helm
-        mv /tmp/helm/linux-amd64/helm ${KUBERNETES_CLIENT_BIN_DIR}
+        curl -sS --location ${KUBERNETES_CLIENT_HELM3_DOWNLOAD_URL} | tar xz -C /tmp/helm
+        mv /tmp/helm/linux-amd64/helm ${KUBERNETES_CLIENT_BIN_DIR}/helm3
+        ln -s ${KUBERNETES_CLIENT_BIN_DIR}/helm ${KUBERNETES_CLIENT_BIN_DIR}/helm3
         rm -rf /tmp/helm
     fi
+
+    if ! type helm2 >/dev/null 2>&1; then
+        _psh_log_info "installing helm2 from: ${KUBERNETES_CLIENT_HELM2_DOWNLOAD_URL}"
+        mkdir -p /tmp/helm
+        curl -sS --location ${KUBERNETES_CLIENT_HELM2_DOWNLOAD_URL} | tar xz -C /tmp/helm
+        mv /tmp/helm/linux-amd64/helm ${KUBERNETES_CLIENT_BIN_DIR}/helm2
+        rm -rf /tmp/helm2
+    fi
+
 
 }
 
@@ -95,17 +107,11 @@ function _psh_kubernetes_client_verify() {
     type kubectl
     kubectl version --client
 
-    type helm
-    helm version
-}
+    type helm3
+    helm3 version --client
 
-
-
-
-
-
-
-
+    type helm2
+    helm2 version --client
 
 
 
